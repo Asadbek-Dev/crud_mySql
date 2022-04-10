00d7282eea944d0edf3b29a825ab55e161acd3ea
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-04-10 19:13:43 +0500

    nodemon completed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-const express=require('express')
-const app=express()
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello Asadbek-Dev");
+});
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
-
-app.listen(3000,()=>{
-    console.log('Running');
-})
+app.listen(3001, () => {
+  console.log("Running");
+});
