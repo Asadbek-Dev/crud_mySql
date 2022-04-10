d02c8d55504a4bdee0a19a2895776ea598a7ef73
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-04-10 22:32:00 +0500

    connected mySql

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
+const mysql=require('mysql');
+const db=mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'maktab40',
+    database:'cruddatabase'
+})
+
 app.get("/", (req, res) => {
-  res.send("Hello Asadbek-Dev");
+    const sqlInsert="SELECT * FROM cruddatabase.movie_reviews;"
+    db.query(sqlInsert,(err,result)=>{
+        res.send("Hello");
+
+    })
 });
 
 app.listen(3001, () => {
