5a1b9481d93111cf0f003838135ab7a0f7fe9c2f
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-04-10 19:07:12 +0500

    completed

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>CRUD Application</h1>
+      <div className="form">
+        <label htmlFor="">Movie:</label>
+        <input type="text" name='movieName'/>
+        <label htmlFor="">Review:</label>
+        <input type="text" name='review'/>
+        <button type='submit'>Submit</button>
+      </div>
     </div>
   );
 }
