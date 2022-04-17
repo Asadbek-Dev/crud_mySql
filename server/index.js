6cde8fe42005c24054c7f031a702aa1456ab45bc
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-04-18 02:40:08 +0500

    worked

--- server/index.js
+++ server/index.js
@@ -5,16 +5,33 @@ const mysql=require('mysql');
 const db=mysql.createPool({
     host:'localhost',
     user:'root',
-    password:'maktab40',
-    database:'cruddatabase'
+    password:'test1234',
+    connectionLimit:10
 })
 
-app.get("/", (req, res) => {
-    const sqlInsert="SELECT * FROM cruddatabase.movie_reviews;"
-    db.query(sqlInsert,(err,result)=>{
-        res.send("Hello");
+// app.get("/", (req, res) => {
+//     const sqlInsert=`INSERT INTO cruddatabase.movie_reviews (movieName, movieReview) VALUES ('Company Inc', 'Highway 37')`;
+//     db.query(sqlInsert,(err,result)=>{
+//         res.send("Hello");
 
-    })
+//     })
+// });
+// var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "test1234",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = `INSERT INTO cruddatabase.movie_reviews (movieName, movieReview) VALUES ('Company Inc', 'Highway 37')`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
 });
 
 app.listen(3001, () => {
