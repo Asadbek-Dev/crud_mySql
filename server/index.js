0c843ba2f143850c55647a782c8d86587311217d
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-04-22 00:13:29 +0500

    bug fixed

--- server/index.js
+++ server/index.js
@@ -1,39 +1,75 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
 
-const mysql=require('mysql');
-// const db=mysql.createPool({
-//     host:'localhost',
-//     user:'root',
-//     password:'test1234',
-//     connectionLimit:10
-// })
+app.use(cors());
+app.use(express.json());
 
-// app.get("/", (req, res) => {
-//     const sqlInsert=`INSERT INTO cruddatabase.movie_reviews (movieName, movieReview) VALUES ('Company Inc', 'Highway 37')`;
-//     db.query(sqlInsert,(err,result)=>{
-//         res.send("Hello");
-
-//     })
-// });
-// var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
+const db = mysql.createConnection({
   user: "root",
+  host: "localhost",
   password: "test1234",
+  database: "employeesystem",
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = `INSERT INTO cruddatabase.movie_reviews (movieName, movieReview) VALUES ('Company Inc', 'Highway 37')`;
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const wage = req.body.wage;
+  db.query(
+    "UPDATE employees SET wage = ? WHERE id = ?",
+    [wage, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
   });
 });
 
 app.listen(3001, () => {
-  console.log("Running");
+  console.log("Yey, your server is running on port 3001");
 });
