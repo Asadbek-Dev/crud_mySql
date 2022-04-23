f71fc1d8507662fc852510a18dbed2cf3cc1e9f9
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-04-23 12:24:51 +0500

    search bar is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,172 +1,58 @@
 import "./App.css";
-import { useState } from "react";
+import{ useState } from "react";
 import Axios from "axios";
 
 function App() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  const [country, setCountry] = useState("");
-  const [position, setPosition] = useState("");
-  const [wage, setWage] = useState(0);
+  const [text, setText] = useState("");
+  const [definition, setDefinition] = useState(0);
 
-  const [newWage, setNewWage] = useState(0);
+  console.log(text);
 
   const [employeeList, setEmployeeList] = useState([]);
 
-  const addEmployee = () => {
-    console.log(name);
-    Axios.post("http://localhost:3001/create", {
-      name: name,
-      age: age,
-      country: country,
-      position: position,
-      wage: wage,
-    }).then(() => {
-      console.log("success");
-      setEmployeeList([
-        ...employeeList,
-        {
-          name: name,
-          age: age,
-          country: country,
-          position: position,
-          wage: wage,
-        },
-      ]);
-    });
-  };
-
-  const getEmployees = () => {
-    Axios.get("http://localhost:3001/employees").then((response) => {
-      console.log(response);
+  const getEmployees = (e) => {
+    e.preventDefault();
+    Axios.get("http://localhost:3001/search").then((response) => {
       setEmployeeList(response.data);
     });
   };
 
-  const updateEmployeeWage = (id) => {
-    Axios.put("http://localhost:3001/update", { wage: newWage, id: id }).then(
-      (response) => {
-        setEmployeeList(
-          employeeList.map((val) => {
-            return val.id == id
-              ? {
-                  id: val.id,
-                  name: val.name,
-                  country: val.country,
-                  age: val.age,
-                  position: val.position,
-                  wage: newWage,
-                }
-              : val;
-          })
-        );
-      }
-    );
-  };
-
-  const deleteEmployee = (id) => {
-    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
-      setEmployeeList(
-        employeeList.filter((val) => {
-          return val.id != id;
-        })
-      );
-    });
-  };
 
   return (
     <div className="App">
-      {/* <div className="information">
-        <label>Name:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        />
-        <label>Age:</label>
-        <input
-          type="number"
-          onChange={(event) => {
-            setAge(event.target.value);
-          }}
-        />
-        <label>Country:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setCountry(event.target.value);
-          }}
-        />
-        <label>Position:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setPosition(event.target.value);
-          }}
-        />
-        <label>Wage (year):</label>
-        <input
-          type="number"
-          onChange={(event) => {
-            setWage(event.target.value);
-          }}
-        />
-        <button onClick={addEmployee}>Add Employee</button>
-      </div>
-      <div className="employees">
-        <button onClick={getEmployees}>Show Employees</button>
-
-        {employeeList.map((val, key) => {
+      <div className="container">
+        <form className="search-box">
+          <input
+            type="text"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            placeholder="Type the word here..."
+            id="inp-word"
+          />
+          <button id="search-btn" onClick={getEmployees}>
+            Search
+          </button>
+        </form>
+        {employeeList.filter((val)=>{
+          if(text==''){
+            return val;
+          }
+          else if(val.first_name.toLowerCase().includes(text.toLowerCase())){
+            return val;
+          }
+        }).map((val,key)=>{
           return (
-            <div className="employee">
-              <div>
-                <h3>Name: {val.name}</h3>
-                <h3>Age: {val.age}</h3>
-                <h3>Country: {val.country}</h3>
-                <h3>Position: {val.position}</h3>
-                <h3>Wage: {val.wage}</h3>
+            <div className="result" id="result" key={key}>
+              <div className="word">
+                <h3 id="searchedText">{val.first_name}</h3>
               </div>
-              <div>
-                <input
-                  type="text"
-                  placeholder="2000..."
-                  onChange={(event) => {
-                    setNewWage(event.target.value);
-                  }}
-                />
-                <button
-                  onClick={() => {
-                    updateEmployeeWage(val.id);
-                  }}
-                >
-                  {" "}
-                  Update
-                </button>
-
-                <button
-                  onClick={() => {
-                    deleteEmployee(val.id);
-                  }}
-                >
-                  Delete
-                </button>
-              </div>
+              <div className="details">{val.job_title}</div>
             </div>
-          );
+          )
         })}
-      </div> */}
-      <div class="container">
-        <div className="search-box">
-            <input type="text" placeholder="Type the word here..." id="inp-word"/>
-            <button  id="search-btn">Search</button>
-        </div>
-        <div className="result" id="result">
-            <div className="word">
-                <h3 id="searchedText"></h3>
-            </div>
-        </div>
-    </div>
+        
+      </div>
     </div>
   );
 }
