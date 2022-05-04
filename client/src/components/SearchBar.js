acfaf626eb92c7a2f75167342694a6f42263620e
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-05-04 20:18:40 +0500

    searchbar is fully

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -3,28 +3,39 @@ import "./SearchBar.css";
 import SearchIcon from "@material-ui/icons/Search";
 import CloseIcon from "@material-ui/icons/Close";
 import Axios from "axios";
+import Select from 'react-select';
 
 function SearchBar({ placeholder}) {
     const [filteredData, setFilteredData] = useState([]);
     const [wordEntered, setWordEntered] = useState("");
     const [words, setWords] = useState([]);
     const [selected, setSelected] = useState([]);
+    const [option, setOption] = useState('eng');
 
     useEffect(() => {
     
         Axios.get("http://localhost:3001/Uzbek").then((response) => {
           setWords(response.data);
         });
-        console.log(words);
       }, []);
-    
 
+      const options = [
+        { value: 'eng', label: 'eng' },
+        { value: 'uz', label: 'uz' },
+      ]
+      console.log(option);
+    
     const handleFilter = (event) => {
     const searchWord = event.target.value;
     setWordEntered(searchWord);
-    const newFilter = words.filter((value) => {
+    if(option==='eng'){
+    var newFilter = words.filter((value) => {
       return value.word_eng.toLowerCase().includes(searchWord.toLowerCase());
-    });
+    })} else if(option==='uz'){
+      var newFilter = words.filter((value) => {
+        return value.word_uz.toLowerCase().includes(searchWord.toLowerCase());
+      })
+    }
 
     if (searchWord === "") {
       setFilteredData([]);
@@ -33,15 +44,15 @@ function SearchBar({ placeholder}) {
     }
   };
 
-    const selectedHandle=(e)=>{
-      console.log('asad',e);
-    }
-
   const clearInput = () => {
     setFilteredData([]);
     setWordEntered("");
     setSelected('')
   };
+
+  const handleChange=(value)=>{
+      setOption(value.value)
+  }
 
   return (
     <div className="search">
@@ -59,22 +70,26 @@ function SearchBar({ placeholder}) {
             <CloseIcon id="clearBtn" onClick={clearInput} />
           )}
         </div>
+        <Select className="select" options={options} defaultValue={{ value: 'eng', label: 'eng' }} onChange={value =>handleChange(value)} />
       </div>
       {filteredData.length != 0 && (
         <div className="dataResult">
           {filteredData.slice(0, 15).map((value, key) => {
             return (
               <a className="dataItem" onClick={()=>setSelected(value)} >
-                <p >{value.word_eng} </p>
+                {option==='eng'?<p >{value.word_eng} </p>:<p >{value.word_uz} </p>}
               </a>
             );
           })}
         </div>
       )}
-      <div className="Result">
+      {option==='eng'?<div className="Result">
           <h2 className="result_word">{selected.word_eng}</h2>
           <p className="result_definition">{selected.definition_eng}</p>
-      </div>
+      </div>:<div className="Result">
+          <h2 className="result_word">{selected.word_uz}</h2>
+          <p className="result_definition">{selected.definition_uz}</p>
+      </div>}
     </div>
   );
 }
